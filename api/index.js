1596922cd32dc75e198dfd3b781658549dfd6860
Daedra <foglzerika@gmail.com>
2020-10-07 09:27:33 -0500

    feat | Create .gitignore file api folder

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
-const app = require('./app')
+const express = require('express')
+
 const PORT = process.env.PORT || 1313
+
+const app = express()
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
